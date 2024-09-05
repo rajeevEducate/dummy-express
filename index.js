64c72bb06138f06f9ac2c2a7ec834ea064cc5d1d
rajeevEducate <rajeev.kumar@educateapp.in>
2024-09-05 13:19:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+import express from "express";
+
+const app = express();
+const PORT = 1000;
+
+app.get("/", (req,res) => {
+    return res.json({
+        success: true,
+        message: `server is up and running on ${PORT}`
+    });
+})
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
